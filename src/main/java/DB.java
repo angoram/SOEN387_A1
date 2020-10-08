7564b1362b4b3bc759bfaa47bed098dd669b37e2
angoram <kor.andrew@outlook.com>
2020-10-08 18:06:10 -0400

    Initial commit

---
+++ src/main/java/DB.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+/**
+ * Just a static class that simulates the "database" for this server. Stores messages.
+ */
+public class DB {
+    public static ArrayList<Message> messages = new ArrayList<Message>();
+}
---
+++ src/main/java/Message.java
@@ -0,0 +1,21 @@
+import java.util.Date;
+
+public class Message {
+    private String postedBy;
+    private String messageContent;
+    private Date datePosted;
+    public Message(String _postedBy, String _messageContent, Date _datePosted){
+        datePosted = _datePosted;
+        messageContent = _messageContent;
+        postedBy = _postedBy;
+    }
+    public String getPostedBy(){
+        return postedBy;
+    }
+    public String getMessageContent(){
+        return messageContent;
+    }
+    public Date getDatePosted(){
+        return datePosted;
+    }
+}
